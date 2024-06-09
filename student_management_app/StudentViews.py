daf117ca1d1cf48ee787c6aaf9647470c17a3248
Sahil Apte <sahil.apte@realpage.com>
2024-06-09 15:06:21 +0530

    test commit

---
+++ student_management_app/StudentViews.py
@@ -0,0 +1,254 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage #To upload Profile Picture
+from django.urls import reverse
+import datetime # To Parse input DateTime into Python Date Time Object
+import pickle
+import re
+import pandas as pd
+import openai
+from openai import api_key, ChatCompletion
+import subprocess
+import time
+from django.conf import settings
+import pythonmonkey as pm
+from student_management_app.models import CustomUser, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, LeaveReportStudent, FeedBackStudent, StudentResult
+
+
+def student_home(request):
+    student_obj = Students.objects.get(admin=request.user.id)
+    total_attendance = AttendanceReport.objects.filter(student_id=student_obj).count()
+    attendance_present = AttendanceReport.objects.filter(student_id=student_obj, status=True).count()
+    attendance_absent = AttendanceReport.objects.filter(student_id=student_obj, status=False).count()
+
+    course_obj = Courses.objects.get(id=student_obj.course_id.id)
+    total_subjects = Subjects.objects.filter(course_id=course_obj).count()
+
+    subject_name = []
+    data_present = []
+    data_absent = []
+    subject_data = Subjects.objects.filter(course_id=student_obj.course_id)
+    for subject in subject_data:
+        attendance = Attendance.objects.filter(subject_id=subject.id)
+        attendance_present_count = AttendanceReport.objects.filter(attendance_id__in=attendance, status=True, student_id=student_obj.id).count()
+        attendance_absent_count = AttendanceReport.objects.filter(attendance_id__in=attendance, status=False, student_id=student_obj.id).count()
+        subject_name.append(subject.subject_name)
+        data_present.append(attendance_present_count)
+        data_absent.append(attendance_absent_count)
+    
+    context={
+        "total_attendance": total_attendance,
+        "attendance_present": attendance_present,
+        "attendance_absent": attendance_absent,
+        "total_subjects": total_subjects,
+        "subject_name": subject_name,
+        "data_present": data_present,
+        "data_absent": data_absent
+    }
+    return render(request, "student_template/student_home_template.html", context)
+
+
+def student_view_attendance(request):
+    student = Students.objects.get(admin=request.user.id) # Getting Logged in Student Data
+    course = student.course_id # Getting Course Enrolled of LoggedIn Student
+    # course = Courses.objects.get(id=student.course_id.id) # Getting Course Enrolled of LoggedIn Student
+    subjects = Subjects.objects.filter(course_id=course) # Getting the Subjects of Course Enrolled
+    context = {
+        "subjects": subjects
+    }
+    return render(request, "student_template/student_view_attendance.html", context)
+
+
+def student_view_attendance_post(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method")
+        return redirect('student_view_attendance')
+    else:
+        # Getting all the Input Data
+        subject_id = request.POST.get('subject')
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+
+        # Parsing the date data into Python object
+        start_date_parse = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
+        end_date_parse = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
+
+        # Getting all the Subject Data based on Selected Subject
+        subject_obj = Subjects.objects.get(id=subject_id)
+        # Getting Logged In User Data
+        user_obj = CustomUser.objects.get(id=request.user.id)
+        # Getting Student Data Based on Logged in Data
+        stud_obj = Students.objects.get(admin=user_obj)
+
+        # Now Accessing Attendance Data based on the Range of Date Selected and Subject Selected
+        attendance = Attendance.objects.filter(attendance_date__range=(start_date_parse, end_date_parse), subject_id=subject_obj)
+        # Getting Attendance Report based on the attendance details obtained above
+        attendance_reports = AttendanceReport.objects.filter(attendance_id__in=attendance, student_id=stud_obj)
+
+        # for attendance_report in attendance_reports:
+        #     print("Date: "+ str(attendance_report.attendance_id.attendance_date), "Status: "+ str(attendance_report.status))
+
+        # messages.success(request, "Attendacne View Success")
+
+        context = {
+            "subject_obj": subject_obj,
+            "attendance_reports": attendance_reports
+        }
+
+        return render(request, 'student_template/student_attendance_data.html', context)
+       
+
+def student_apply_leave(request):
+    student_obj = Students.objects.get(admin=request.user.id)
+    leave_data = LeaveReportStudent.objects.filter(student_id=student_obj)
+    context = {
+        "leave_data": leave_data
+    }
+    return render(request, 'student_template/student_apply_leave.html', context)
+
+
+def student_apply_leave_save(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method")
+        return redirect('student_apply_leave')
+    else:
+        leave_date = request.POST.get('leave_date')
+        leave_message = request.POST.get('leave_message')
+
+        student_obj = Students.objects.get(admin=request.user.id)
+        try:
+            leave_report = LeaveReportStudent(student_id=student_obj, leave_date=leave_date, leave_message=leave_message, leave_status=0)
+            leave_report.save()
+            messages.success(request, "Applied for Leave.")
+            return redirect('student_apply_leave')
+        except:
+            messages.error(request, "Failed to Apply Leave")
+            return redirect('student_apply_leave')
+
+
+def student_feedback(request):
+    student_obj = Students.objects.get(admin=request.user.id)
+    feedback_data = FeedBackStudent.objects.filter(student_id=student_obj)
+    context = {
+        "feedback_data": feedback_data
+    }
+    return render(request, 'student_template/student_feedback.html', context)
+
+
+def student_feedback_save(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method.")
+        return redirect('student_feedback')
+    else:
+        feedback = request.POST.get('feedback_message')
+        student_obj = Students.objects.get(admin=request.user.id)
+
+        try:
+            add_feedback = FeedBackStudent(student_id=student_obj, feedback=feedback, feedback_reply="")
+            add_feedback.save()
+            messages.success(request, "Feedback Sent.")
+            return redirect('student_feedback')
+        except:
+            messages.error(request, "Failed to Send Feedback.")
+            return redirect('student_feedback')
+
+
+def student_profile(request):
+    user = CustomUser.objects.get(id=request.user.id)
+    student = Students.objects.get(admin=user)
+
+    context={
+        "user": user,
+        "student": student
+    }
+    return render(request, 'student_template/student_profile.html', context)
+
+
+def student_profile_update(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method!")
+        return redirect('student_profile')
+    else:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+
+        try:
+            customuser = CustomUser.objects.get(id=request.user.id)
+            customuser.first_name = first_name
+            customuser.last_name = last_name
+            if password != None and password != "":
+                customuser.set_password(password)
+            customuser.save()
+
+            student = Students.objects.get(admin=customuser.id)
+            student.address = address
+            student.save()
+            
+            messages.success(request, "Profile Updated Successfully")
+            return redirect('student_profile')
+        except:
+            messages.error(request, "Failed to Update Profile")
+            return redirect('student_profile')
+
+
+def student_view_result(request):
+    student = Students.objects.get(admin=request.user.id)
+    student_result = StudentResult.objects.filter(student_id=student.id)
+    context = {
+        "student_result": student_result,
+    }
+    return render(request, "student_template/student_view_result.html", context)
+
+def student_course_recomendation(request):
+    courses_data = pickle.load(open('student_management_app/templates/student_template/courses.pkl', 'rb'))
+    similarity = pickle.load(open('student_management_app/templates/student_template/similarity.pkl', 'rb'))
+    def recommend(course):
+        indexValue = course_list.index(course)
+        distances = sorted(list(enumerate(similarity[indexValue])), reverse=True, key=lambda x: x[1])
+        recommended_course_names = []
+        for i in distances[1:7]:
+            course_name = courses_data.iloc[i[0]]['course_name']
+            recommended_course_names.append(course_name)
+        return recommended_course_names
+
+    course_list = courses_data['course_name'].tolist()
+    selected_course = 'Front-End Web Development with React'  # Default selected course
+    
+    if request.method == 'POST':
+        selected_course = request.POST.get('course_dropdown')
+
+    recommended_course_names = recommend(selected_course)
+
+    context = {
+        'courses_list': course_list,  # Changed variable name here
+        'recommended_courses': recommended_course_names,
+    }
+    return render(request, "student_template/student_course_recomendation.html", context)
+
+def student_askgpt(request):
+    if request.method == 'POST':
+        try:
+            msg = request.POST.get('student_askgpt') 
+            print(msg)
+            result=""
+            with open('input.txt', 'w') as file:
+                file.write(msg)
+            subprocess.run(["node", "student_management_app/script.js"])
+            with open('output.txt', 'r') as file:
+                result = file.read()
+            context = {"student_askgpt": result}
+            return render(request, "student_template/student_askgpt.html", context)
+        except Exception as e:
+            error_message = str(e)
+            print(error_message)
+            context = {"error_message": error_message}
+            return render(request, "student_template/student_askgpt.html", context)        
+    else:
+        # Handle cases where the request method is not POST
+        error_message = "Invalid request method"
+        context = {"error_message": error_message}
+        print('Not Entered')
+        return render(request, "student_template/student_askgpt.html", context)
---
+++ student_management_app/script.js
@@ -0,0 +1,65 @@
+// node --version # Should be >= 18
+// npm install @google/generative-ai
+
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+  
+  const MODEL_NAME = "gemini-1.0-pro";
+  const API_KEY = "YOUR_API_KEY";
+    const fs = require('fs');
+const promptValue = fs.readFileSync('input.txt', 'utf8');
+  console.log('Prompt value:', promptValue);
+  
+  async function runChat() {
+
+
+    const genAI = new GoogleGenerativeAI("AIzaSyBEE0DUx7roECOtwqgZi3XUJZCwR8LT2fs");
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  
+    const generationConfig = {
+      temperature: 0.9,
+      topK: 1,
+      topP: 1,
+      maxOutputTokens: 2048
+    };
+  
+    const safetySettings = [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+    ];
+  
+    const chat = model.startChat({
+      generationConfig,
+      safetySettings,
+      history: [
+      ],
+    });
+    const result = await chat.sendMessage(promptValue);
+    const response = result.response.text();
+    fs.writeFile('output.txt', response, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log('Text written to file');
+    });
+  }
+  
+  runChat();
